d9d498865f78835fe2452ff3920690863d232a92
“Alfred <osangalfred@gmail.com>
2024-10-28 17:25:33 +0100

    UI update

--- components/Testimonal.jsx
+++ components/Testimonal.jsx
@@ -167,69 +167,73 @@ const Testimonal = () => {
 
             </div>
 
-            {/* <div className='bg-white h-[1000px]flex justify-center items-center'>
+            <div className='bg-white h-[500px]'>
+
+
+                <div className='flex justify-center items-center py-12'>
 
 
 
-                <div
-                    className=" h-[150vh] md:h-[400px] w-[700px] rounded-2xl "
-                    style={{
-                        backgroundImage: "url('/trust.png')",
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                        backgroundRepeat: 'no-repeat',
-                        height: '',
-                    }}
-                >
-                    <div className='container mx-auto p-10 '>
+                    <div
+                        className=" h-[800px] md:h-[350px] w-[700px] rounded-3xl "
+                        style={{
+                            backgroundImage: "url('/trust.png')",
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center',
+                            backgroundRepeat: 'no-repeat',
+                            height: '',
+                        }}
+                    >
+                        <div className=' p-10 '>
 
 
-                        <div className='flex flex-col-reverse md:flex-row justify-center  gap-10 items-center'>
-                            <div>
-                                <Image
-                                    src="/trustMobile.png"
-                                    alt="Background"
-                                    width="300"
-                                    height="600"
-                                    layout=""
-                                    objectFit=""
-                                    quality={100} // Optional: Adjust quality
-                                    className="-z-10" // Ensure it stays in the background
-                                />
+                            <div className='flex flex-col-reverse md:flex-row justify-center  gap-7 items-center'>
+                              
+
+                                <div className='space-y-4'>
+                                   
+
+
+                                    <p className='text-white text-3xl font-extrabold w-[380px]'>
+                                    Are you ready to start?
+                                    </p>
+
+                                    <p className='text-white max-w-[400px] font-extralight'>
+                                    Personalize your settings, follow your progress, archive your highlights and notes automatically Glose is the ultimate reading 
+                                    </p>
+
+                                    <div className='flex relative'>
+                                        <button className='text-white bg-green-500 rounded-full px-7 py-2 text-sm font-light'>
+                                            Get Started
+                                        </button>
+                                        <div className='bg-white w-[35px] h-[35px] rounded-full absolute top-0 left-28 flex justify-center items-center'><MdArrowOutward className='text-gray-700' /></div>
+                                    </div>
+
+
+                                </div>
+
+                                <div>
+                                    <Image
+                                        src="/pp.png"
+                                        alt="Background"
+                                        width="450"
+                                        height="950"
+                                        layout=""
+                                        objectFit=""
+                                        quality={100} // Optional: Adjust quality
+                                        className="mt-[38px]" // Ensure it stays in the background
+                                    />
+
+                                </div>
 
                             </div>
 
-                            <div className='space-y-4'>
-                                <div className='bg-white px-3 py-2 rounded-full bg-opacity-5 w-[150px]'>
-                                    <p className='text-white font-extralight text-[11px]'>🔥 TRUSTWORTHNESS</p>
-                                </div>
-
-
-                                <p className='text-white text-3xl font-extrabold w-[380px]'>
-                                    WE VALUE YOUR TRUST AND SECURITY
-                                </p>
-
-                                <p className='text-white max-w-[400px] font-extralight'>
-                                    Our mission is to make finance more accessible, transparent, and secure for everyone. With cutting.
-                                </p>
-
-                                <div className='flex relative'>
-                                    <button className='text-white bg-green-500 rounded-full px-4 py-2 text-sm font-light'>
-                                        Open Account
-                                    </button>
-                                    <div className='bg-white w-[35px] h-[35px] rounded-full absolute top-0 left-28 flex justify-center items-center'><MdArrowOutward className='text-gray-700' /></div>
-                                </div>
-
-
-                            </div>
 
                         </div>
-
-
                     </div>
                 </div>
 
-            </div> */}
+            </div>
         </>
     )
 }
